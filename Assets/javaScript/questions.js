1743a7ab600760a4856b5e83c81c68b79131327f
Kamille <tramia25@gmail.com>
2021-07-01 05:11:38 -0400

    updated question javascript

---
+++ Assets/javaScript/questions.js
@@ -0,0 +1,114 @@
+const questions = [
+  {
+    question: 'Which one is a looping structure in JavaScript?',
+    answers: [
+      { text: 'All of the Above', correct: true},
+      { text:  'For', correct: false},
+      { text: 'while', correct: false },
+      { text: 'do-while loops', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'What are the two basic groups of data types in JavaScript?',
+    answers: [
+      { text: 'Primitive and Attributes', correct: false},
+      { text:  'Primitive and Reference', correct: true},
+      { text: 'Reference types and Attributes', correct: false },
+      { text: 'None of the above', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'Commonly used data types DO NOT include',
+    answers: [
+      { text: 'Aerts', correct: true},
+      { text:  'strings', correct: false},
+      { text: 'booleans', correct: false },
+      { text: 'numbers', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'The condition in an if / else statement is enclosed within ____.',
+    answers: [
+      { text: 'parentheses', correct: true},
+      { text:  'curly brackets', correct: false},
+      { text: 'square brackets', correct: false },
+      { text: 'quotes', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'What are arrays used to store in JavaScript?',
+    answers: [
+      { text:  'other arrays', correct: false},
+      { text: 'booleans', correct: false },
+      { text: 'numbers and strings', correct: false},
+      { text: 'All of the Above', correct: true}
+    
+    ]
+    
+  },
+  {
+    question: 'What very useful tool is used for printing content to the debugger and debugging?',
+    answers: [
+      { text:  'terninal', correct: false},
+      { text: 'console.log', correct: true },
+      { text: 'whie loops', correct: false},
+      { text: 'debugger', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'What are the two basic groups of data types in JavaScript?',
+    answers: [
+      { text:  'Primitive and attributel', correct: false},
+      { text: 'Reference types and attribute', correct: true },
+      { text: 'Primitive and reference types', correct: true},
+      { text: 'none of the above', correct: false}
+    
+    ]
+    
+  },
+  
+  {
+    question: 'In JavaScript what is the data type of variables?',
+    answers: [
+      { text:  'function data', correct: false},
+      { text: 'object data', correct: true },
+      { text: 'none of the above', correct: true},
+      { text: 'all of the above', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'What encloses strings when assigned a variable?',
+    answers: [
+      { text:  'curly brackets', correct: false},
+      { text: 'commas', correct: false },
+      { text: 'quotes', correct: true},
+      { text: 'parentheses', correct: false}
+    
+    ]
+    
+  },
+  {
+    question: 'What type of pop-up boxes are available in JavaSript?',
+    answers: [
+      { text:  'confirm', correct: false},
+      { text: 'alert', correct: false },
+      { text: 'prompt', correct: false},
+      { text: 'All of the above', correct: true}
+    
+    ]
+    
+  },
+  
+]
